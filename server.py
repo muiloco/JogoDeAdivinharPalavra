4adcd82c00daf47b935b8bf591c460af6113b9c9
unknown <muiloco17@gmail.com>
2019-05-23 00:01:38 -0300

    Funções do Server

--- server.py
+++ server.py
@@ -8,8 +8,21 @@ class Palavra(object):
         self.palavra = palavra
         self.dica = dica
 
+    def conferirLetra(letra):
+        if letra in self.palavra:
+            return True
+
+    def conferirPalavra(palavra):
+        if palavra == self.palavra:
+            return True
+        else:
+            return False
+    
+palavra = input("Digite a sua palavra:")
+dica = input("Digite sua dica:")
+
 deamon = Pyro4.Daemon()
 ns = Pyro4.locateNS()
-uri = deamon.register(Palavra)
+uri = deamon.register(Palavra(palavra,dica))
 ns.register("palavra",uri)
 deamon.requestLoop()
